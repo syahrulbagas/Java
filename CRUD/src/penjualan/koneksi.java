ebc2109e9763d08e6ff3f9346bc2bd728c08b52b
Syahrul Bagas <71810571+syahrulbagas@users.noreply.github.com>
2022-11-12 19:34:08 +0700

    Add files via upload

---
+++ CRUD/src/penjualan/koneksi.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package penjualan;
+import java.sql.*;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Hp
+ */
+public class koneksi {
+    private static Connection KoneksiDatabse;
+    public static Connection koneksiDB() throws SQLException {
+        try {
+            String DB ="jdbc:mysql://localhost/penjualan";
+            String user="root";
+            String pass="";
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            KoneksiDatabse = (Connection) DriverManager.getConnection(DB,user,pass);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Tidak ada koneksi","Error",
+            JOptionPane.INFORMATION_MESSAGE);
+            System.err.println(e.getMessage());
+            System.exit(0);
+        }
+         return KoneksiDatabse;
+    }
+}
